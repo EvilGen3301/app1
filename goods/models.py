94b373c4c1802fbdde187fda6228ecb15a76d9fe
EvilGen3301 <tablock18@gmail.com>
2024-05-15 22:18:47 +0400

    Added fixtures for goods

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,33 @@
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="სახელი")
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
+
+    class Meta:
+        db_table = "category"
+        verbose_name = "კატეგორია"
+        verbose_name_plural = "კატეგორიები"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="სახელი")
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
+    description = models.TextField(blank=True, null=True, verbose_name="აღწერა")
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name="გამოსახულება")
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name="ფასი")
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name="ფასდაკლება % -ში")
+    quantity = models.PositiveIntegerField(default=0, verbose_name='რაოდენობა')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name="კატეგორია")
+    class Meta:
+        db_table = "product"
+        verbose_name = "პროდუქტი"
+        verbose_name_plural = "პროდუქტები"
+
+    def __str__(self) -> str:
+        return f'{self.name} რაოდენობა - {self.quantity}'
--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import Categories
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context: dict[str, str] = {
         "title": "Home - მთავარი",
-        "content": "მაღაზია HOME"
+        "content": "მაღაზია HOME",
+        "categories": categories
 }
 
     return render(request, 'main/index.html', context)
